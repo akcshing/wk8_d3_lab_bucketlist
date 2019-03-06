2fd13f0eebffe3842a4489778c4cc8d379c427d7
Alexkcshing <alexkcshing@gmail.com>
2019-03-06 10:31:40 +0000

    start creating router

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const parser = require("body-parser");
 const path = require("path");
+const parser = require("body-parser");
+const createRouter = require("./helpers/create_router.js");
 
 const publicPath = path.join(__dirname, "../client/public");
 app.use(express.static(publicPath));
 app.use(parser.json());
+
+
+
+app.use("/api/bucketlist", createRouter)
+
+
+
+app.listen(3000, function () {
+  console.log(`listening on port ${this.address().port}`);
+});
