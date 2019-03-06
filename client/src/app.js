664c64b14bf2a954aa1068cf7c7409645c0df7c5
Alexkcshing <alexkcshing@gmail.com>
2019-03-06 13:18:44 +0000

    finished bucketlist model

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,8 @@
 const Bucketlist = require('./models/bucketlist');
 
 document.addEventListener("DOMContentLoaded", () => {
-  const bucketlist = new Bucketlist("http://localhost:3000/api/bucketlist")
+  const bucketlist = new Bucketlist("http://localhost:3000/api/bucketlist");
+  bucketlist.getData();
+  console.log('loaded')
 
 })
--- client/src/models/bucketlist.js
+++ client/src/models/bucketlist.js
@@ -1,10 +1,37 @@
 const RequestHelper = require("../helpers/request_helper");
+const PubSub = require('../helpers/pub_sub.js')
 
 const Bucketlist = function (url) {
   this.url = url
   this.request = new RequestHelper(this.url)
 }
 
+Bucketlist.prototype.publish = function (bucketlist) {
+  PubSub.publish("Bucketlist:data-ready", bucketlist)
+};
 
+ Bucketlist.prototype.getData = function () {
+   this.request.get()
+    .then(request => this.publish(request))
+    .catch(console.error)
+ };
+
+ Bucketlist.prototype.addItem = function (newItem) {
+   this.request.post(newItem)
+   .then(request => this.publish(request))
+   .catch(console.error)
+ };
+
+ Bucketlist.prototype.deleteItem = function (id) {
+   this.request.delete(id)
+   .then(request => this.publish(request))
+   .catch(console.error)
+ };
+
+ Bucketlist.prototype.updateItem = function (id, newItem) {
+   this.request.update(id, newItem)
+   .then(request => this.publish(request))
+   .catch(console.error)
+ };
 
 module.exports = Bucketlist;
